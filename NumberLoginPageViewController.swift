f9fa4408ab99ea2d5cd993dd7303fc0187f40cd4
meetmangukiya2710 <meetmangukiya104@gmail.com>
2024-05-24 12:06:35 +0530

    Pod Remove

---
+++ NumberLoginPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NumberLoginPageViewController.swift
+//  Master Calculation
+//
+//  Created by R95 on 23/05/24.
+//
+
+import UIKit
+
+class NumberLoginPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
